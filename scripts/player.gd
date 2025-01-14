2165f6ca12c82cb22ef56e84be5cc65d13c75bd4
Mark Nair <47827647+marknair@users.noreply.github.com>
2025-01-14 10:02:10 -0600

    Clamping

---
+++ scripts/player.gd
@@ -0,0 +1,23 @@
+extends CharacterBody2D
+
+var speed = 1000
+
+func _physics_process(delta):
+	velocity = Vector2(0, 0)
+	if Input.is_action_pressed("move_right"):
+		velocity.x = speed
+	if Input.is_action_pressed("move_left"):
+		velocity.x = -speed
+	if Input.is_action_pressed("move_up"):
+		velocity.y = -speed
+	if Input.is_action_pressed("move_down"):
+		velocity.y = speed
+	move_and_slide()
+	
+	var screen_size = get_viewport_rect().size
+
+	global_position = global_position.clamp(Vector2(0,0), screen_size)
+	
+	var newSpeed = .6
+	var a = clampf(newSpeed, 1.0, 20.5)
+	print(a)
