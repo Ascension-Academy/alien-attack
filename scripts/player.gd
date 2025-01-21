4b2c0ecfa28e2bae731f992cbee1ad071ff18a9a
Mark Nair <47827647+marknair@users.noreply.github.com>
2025-01-21 09:49:17 -0600

    Rocket

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,9 +1,16 @@
 extends CharacterBody2D
 
 var speed = 1000
+var rocket_scene = preload("res://scenes/rocket.tscn")
+@onready var rocket_container = $"RocketContainer"
+
+func _ready():
+	pass
 
 func _physics_process(delta):
 	velocity = Vector2(0, 0)
+	if Input.is_action_just_pressed("shoot"):
+		shoot()
 	if Input.is_action_pressed("move_right"):
 		velocity.x = speed
 	if Input.is_action_pressed("move_left"):
@@ -17,7 +24,17 @@ func _physics_process(delta):
 	var screen_size = get_viewport_rect().size
 
 	global_position = global_position.clamp(Vector2(0,0), screen_size)
+
+
+func shoot():
+	# Make the instance of the rocket
+	var rocket_instance = rocket_scene.instantiate()
+	add_sibling(rocket_instance)
+	rocket_instance.position = self.position
+	rocket_instance.position.x += 75
 	
-	var newSpeed = .6
-	var a = clampf(newSpeed, 1.0, 20.5)
-	print(a)
+	#rocket_container.add_child(rocket_instance)
+	#
+	## Set the position of the instance
+	#rocket_instance.global_position = global_position
+	#rocket_instance.global_position.x += 75
